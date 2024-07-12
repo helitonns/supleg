73f51fe9b4c09a509797384825e5fa238136bd9c
Heliton Nascimento <heliton.nascimento@ad.al.rr.leg.br>
2024-07-12 11:12:36 -0400

    Teste de websocket

--- src/main/java/br/leg/rr/al/supleg/websocket/ContentSecurityPolicyFilter.java
+++ src/main/java/br/leg/rr/al/supleg/websocket/ContentSecurityPolicyFilter.java
@@ -16,7 +16,8 @@ public class ContentSecurityPolicyFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if (response instanceof HttpServletResponse) {
             HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-            httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' ws://webapps.homol.al.rr.leg.br; font-src 'self' data:");
+            //httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' ws://webapps.homol.al.rr.leg.br; font-src 'self' data:");
+            httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' ws://ws.ifelse.io; font-src 'self' data:");
         }
         chain.doFilter(request, response);
     }
